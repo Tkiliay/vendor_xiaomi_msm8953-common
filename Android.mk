3d80749bd945bb28607a823527c00d90f5d50e66
Tkiliay <sackclouths@outlook.com>
2023-08-08 23:25:00 +0800

    msm8953-common: Add tiffany support
    Change-Id: I924d84402fb99be0055d74ab5da5e275aefb700f

--- Android.mk
+++ Android.mk
@@ -4,6 +4,6 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter daisy mido sakura tissot vince ysl,$(TARGET_DEVICE)),)
+ifneq ($(filter daisy mido sakura tissot tiffany vince ysl,$(TARGET_DEVICE)),)
 
 endif
